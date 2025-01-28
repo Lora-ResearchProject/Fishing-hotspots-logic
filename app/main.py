f887ab291eca09e6a82354d9efa9f7ec7aeef1be
udanthaisuru@gmail.com <udanthaisuru@gmail.com>
2025-01-29 00:34:13 +0530

    Update the just GET request

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from apscheduler.schedulers.background import BackgroundScheduler
 from .apis import hotspots, vessels, suggestions
 
@@ -24,3 +25,11 @@ def stop_scheduler():
 @scheduler.scheduled_job("interval", minutes=5)
 def check_vessel_activity():
     pass
+
+
+# Custom exception handler for 404 errors
+@app.exception_handler(404)
+async def custom_404_handler(request: Request, exc):
+    if request.method == "GET":
+        return JSONResponse(content={"message": "Sorry!, Please make a valid Request!"}, status_code=404)
+    return JSONResponse(content={"message": "Not Found"}, status_code=404)
